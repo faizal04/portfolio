f5a49b6a7817f57da6e709efac0aed70d90eb148
faizal04 <100855720+faizal04@users.noreply.github.com>
2023-02-28 15:00:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+// console.log("hello world")
+// console.log("faisal harray")
+// function myFunction() {
+//   document.getElementById("the").style.display = "none";
+// }
+// function myFunction(element){
+//     var hide = element.style.backgroundColor;
+//     if(hide == "red"){
+//         element.style.backgroundColor = "green";
+//     }
+//     else{
+//         element.style.backgroundColor = "red";
+//     }
+// }
+// document.getElementById("about").addEventListener("click",function()
+// {
+//     var hide=document.getElementById("p");
+//     if(hide.style.display == "block"){
+//         hide.style.display = "none";
+//     }
+//     else{
+//         hide.style.display = "block";
+//     }
+// })
+// document.getElementById("beginning").addEventListener("click",function()
+// {
+//     var hide=document.getElementById("p2");
+//     if(hide.style.display == "none"){
+//         hide.style.display = "block";
+//     }
+//     else{
+//         hide.style.display = "none";
+//     }
+// })
+
+
+// document.getElementById("first").addEventListener("click",function()
+// {
+//     var hide=document.getElementById("p3");
+//     if(hide.style.display == "none"){
+//         hide.style.display = "block";
+//     }
+//     else{
+//         hide.style.display = "none";
+//     }
+// })
+
+
+// document.getElementById("career").addEventListener("click",function()
+// {
+//     var hide=document.getElementById("p4");
+//     if(hide.style.display == "block"){
+//         hide.style.display = "none";
+//     }
+//     else{
+//         hide.style.display = "block";
+//     }
+// })
